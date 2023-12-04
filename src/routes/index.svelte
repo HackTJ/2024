6e9e42781b15681c2ec1abf5f9150add65824be7
Krishnan Shankar <krishnans2006@gmail.com>
2023-12-04 11:14:06 -0500

    Add MLH member event badge

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -41,6 +41,17 @@
 </svelte:head>
 
 <Navbar {navItems} />
+<a
+	id="mlh-trust-badge"
+	style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
+	href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
+	target="_blank"
+	><img
+		src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
+		alt="Major League Hacking 2024 Hackathon Season"
+		style="width:100%"
+	/></a
+>
 <!-- Home screen -->
 <div class="h-[calc(100vh_-_8rem)] min-h-[512px]">
 	<div
