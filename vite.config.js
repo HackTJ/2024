6a8a83b948f1fd0ab43a270512664999a880158c
Krishnan Shankar <krishnans2006@gmail.com>
2024-02-22 18:20:09 -0500

    Automatically open website homepage (when using `vite dev`)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		open: '/2024'
+	}
 };
 
 export default config;
